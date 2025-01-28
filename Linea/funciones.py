10dfd1883bdf1f03ec543f3cf7598be5654cae47
DonaOne <ianrodriguez_32@outlook.com>
2025-01-28 15:09:54 -0700

    Cambie los dos archivos, ahora los 2 jalan

--- Linea/funciones.py
+++ Linea/funciones.py
@@ -1,30 +1,31 @@
 '''
-Funciones auxiliares para el programa linea
+Funciones auxiliares para el programa "Linea"
+
 '''
 import matplotlib.pyplot as plt
 
-def calcular_y(x,m,b):
-    '''
-        Calcula "y" de acuerdo a la pendiente "m" y 
-        el punto de interseccion en y "b"
-        Retorna el valor de "y"
-    '''
-    return m*x+b
-if __name__ == "__main__":
-    x = 0
-    m = 3
-    b = 2
-    y = calcular_y(x,m,b)
-    if y == 2:
-        print("Prueba exitosa")
-    else:
-        print("Prueba fallida")
-def grafica_linea(X:list,Y:list, m:float, b:float):
-    '''
-    '''
+def calcular_y (x,m,b):
+    ''' Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b". Retorna el valor de "y" '''
+    return (m*x)+b
+
+def grafica_linea(X: list, Y:list, m:float, b:float):
     plt.plot(X,Y)
-    plt.title(f'Linea recta con pendiente={m} e interseccion en y={b}')
+    plt.title(f"Línea recta con pendiente={m}")
     plt.xlabel('x')
     plt.ylabel('y')
     plt.grid()
-    plt.show
+    plt.show()
+    
+    
+def test_linea():
+    y = calcular_y(0.0, 2.0, 3.0)
+    return y
+
+
+
+
+if __name__ == "__main__":  
+    if test_linea() == 3.0:
+        print("Prueba exitosa")
+    else:
+        print("Prueba fallida")
