e4d138f992b373f7279e89381f02104a3af94f52
DonaOne <ianrodriguez_32@outlook.com>
2025-01-30 14:35:25 -0700

    Agregue la funcion estrategia de usuario

--- gato/tablero.py
+++ gato/tablero.py
@@ -23,15 +23,28 @@ def ia(simbolos:dict):
         if simbolos[x] not in ['X','O']:
             simbolos[x] = 'O'
             ocupado = False
-        
-    
+
+def usuario(simbolos:dict):
+    '''Estrategia de Usuario'''
+    ocupado = True
+    lista_numeros = [str(i) for i in range(1,10)] #del 1 al 9]
+    while ocupado is True:
+        x = input('Elija un numero del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X','O']:
+                simbolos[x] = 'X'
+                ocupado = False
+            else:
+                print('Esa casilla esta ocupada')    
+
 if __name__ == '__main__':
         numeros = [str(i) for i in range(1,10)]
         dsimbolos = {x:x for x in numeros}
         dibuja_tablero(dsimbolos)
         ia(dsimbolos)
         dibuja_tablero(dsimbolos)
-        
+        usuario(dsimbolos)
+        dibuja_tablero(dsimbolos)
         '''
         x = random.choice(numeros)
         numeros.remove(x)
