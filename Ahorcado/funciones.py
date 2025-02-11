d66eed677a671cdb614b3cc892d386329c0847f0
DonaOne <ianrodriguez_32@outlook.com>
2025-02-11 14:32:08 -0700

    agregue las funciones de desplegar plantillas

--- Ahorcado/funciones.py
+++ Ahorcado/funciones.py
@@ -8,7 +8,26 @@ def carga_archivo_texto(archivo:str)->list:
         oraciones = file.readlines()
     return oraciones
 
+
+def carga_pantillas(nombre_plantilla:str)->dict:
+    '''Carga una plantilla y regresa una lista con las palabras'''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+
+
+def despliega_plantila(diccionario:list, nivel:int):
+    '''Despliega una plantilla del juego'''
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+            
 if __name__ == '__main__':
-   lista = carga_archivo_texto('./plantillas/plantilla-0.txt')
-   for elemento in lista:
-       print(elemento)
+    plantillas = carga_pantillas('plantilla')
+    despliega_plantila(plantillas, 5)
+    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    print(lista_oraciones[120:150])
+    texto = ' '.join(lista_oraciones[120:150])
