6dd908fae129853280ae030d7a4043814b0165e3
DonaOne <ianrodriguez_32@outlook.com>
2025-02-19 14:46:59 -0700

    Creamos la clase Sport

--- games/Athlete.py
+++ games/Athlete.py
@@ -1,25 +1,24 @@
 class Athlete:
-    '''
-    Athlete class, with only name attribute
-    '''
-    def __init__(self, name:str):
+    """Athlete class"""
+    def _init_(self, name):
         self.name = name
-        
-    def __str__(self):
+
+    def _str_(self):
         return f"Athlete: {self.name}"
     
-    def __repr__(self):
-        return f"Athlete: {self.name}"
+    def _repr_(self):
+        return f"Athlete('{self.name}')"
     
     def display(self):
         print(f"{self.name}")
-        
-if __name__ == "__main__":
+
+if _name_ == "_main_":
     a = Athlete("Ana G.")
     a.display()
     print(a)
     print(repr(a))
     print(f"a: {id(a)}")
+    
     b = eval(repr(a))
     print(b)
-    print(f"b: {id(b)}")
+    print(f"b: {id(b)}")
---
+++ games/Sport.py
@@ -0,0 +1,54 @@
+"""Clase Sport """
+class Sport:
+    """Clase para presentar un deporte"""
+    def __init__(self, name:str, players:int, league:str):
+        self.name = name
+        if isinstance(players, int):
+            #Verificamos que sea un entero
+            self.players = players
+        else:
+            self.players = int(players)
+        self.league = league
+    
+    def __str__(self):
+        return f"Sport: {self.name}, Players: {self.players}, League: {self.league}"
+    
+    def __repr__(self):
+        return f"Sport(name='{self.name}', players={self.players}, league='{self.league}')"
+    
+    def to_json(self)->dict:
+        """Metodo para representar la clase como diccionario"""
+        return {"name":self.name, "players":self.players, "league":self.league}
+    
+if __name__ == "__main__":
+    s = Sport("Soccer", 22, "FIFA")
+    print(s)
+    print(repr(s))
+    print(s.to_json())
+    nfl = Sport("Football", 11, "NFL")
+    lmp = Sport("Baseball", 9, "LMP")
+    mlb = Sport("Baseball", 9, "MLB")
+    lmx = Sport("Soccer", 22, "Liga MX")
+    nba = Sport("Basketball", 5, "NBA")
+    lista_deportes = [nfl, lmp, mlb, lmx, nba,s]
+    archivo_deportes = "deportes.txt"
+    with open(archivo_deportes, "w") as file:
+        for deporte in lista_deportes:
+            file.write(repr(deporte) + "\n")
+    sport_list = []
+    with open(archivo_deportes, "r") as file:
+        for line in file:
+            d = eval(line)
+            sport_list.append(d)
+    print(sport_list)
+    print(sport_list[0].to_json())
+    #Escribiremos e; archivo en formato JSON
+    import json
+    archivo_json = "deportes.json"
+    with open(archivo_json, "w") as file:
+        for deporte in sport_list:
+            json.dump(deporte.to_json(), file)
+            file.write("\n")
+    #Leemos el archivo JSON
+    sport_list_json = []
+    
